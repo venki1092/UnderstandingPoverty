151d2c27a3f3275e2e49a47d8db747c8587043c8
Sam Siu <samsiu@Sams-MacBook-Pro-2.local>
2016-08-17 19:48:30 -0700

    Jobs: Created JobsActivity

---
+++ app/src/main/java/com/example/venki/up/activities/jobs/JobsActivity.java
@@ -0,0 +1,15 @@
+package com.example.venki.up.activities.jobs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.venki.up.R;
+
+public class JobsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jobs);
+    }
+}
