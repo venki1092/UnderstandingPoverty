b905b02f7a859204ea63a840d159f6601968a858
Sam Siu <samsiu@Sams-MacBook-Pro-2.local>
2016-08-17 20:12:09 -0700

    Jobs: Added Dagger2 with Jobs Retrofit Instantiation

--- app/src/main/java/com/example/venki/up/activities/jobs/JobsActivity.java
+++ app/src/main/java/com/example/venki/up/activities/jobs/JobsActivity.java
@@ -1,11 +1,42 @@
 package com.example.venki.up.activities.jobs;
 
+import android.content.SharedPreferences;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.venki.up.R;
+import com.example.venki.up.adapters.JobsRVAdapter;
+import com.example.venki.up.model.job.Indeed;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import retrofit2.Retrofit;
 
 public class JobsActivity extends AppCompatActivity {
+
+    private static final String TAG = "JOBS_FRAGMENT";
+    private Button submitButton;
+    private EditText cityEditText;
+    private EditText jobTitleEditText;
+    private RecyclerView jobRecyclerView;
+    private SwipeRefreshLayout jobsSwipeRefreshLayout;
+    private Indeed indeed;
+
+    private LinearLayoutManager linearLayoutManager;
+    private GridLayoutManager gridLayoutManager;
+    private JobsRVAdapter jobsRVAdapter;
+    SharedPreferences sharedPreferences;
+
+    @Inject
+    @Named("Jobs")
+    Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
