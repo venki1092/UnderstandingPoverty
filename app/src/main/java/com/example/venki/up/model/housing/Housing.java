6bca9690263dd49e274c9f87e8840411cf1eb938
Sam Siu <samsiu@Sams-MacBook-Pro-2.local>
2016-08-19 19:00:11 -0700

    Housing: Added HousingRVAdapter, Housing Model, Housing Cardview xml

---
+++ app/src/main/java/com/example/venki/up/model/housing/Housing.java
@@ -0,0 +1,164 @@
+package com.example.venki.up.model.housing;
+
+/**
+ * Created by samsiu on 7/10/16.
+ */
+public class Housing {
+
+    String services;
+    String languages;
+    String adr2;
+    String city;
+    String adr1;
+    String agc_ADDR_LONGITUDE;
+    String parentid;
+    String county_NME;
+    String phone2;
+    String mailingadr1;
+    String mailingadr2;
+    String mailingcity;
+    String mailingzipcd;
+    String mailingstatecd;
+    String state_NME;
+    String state_FIPS_CODE;
+    String faithbased;
+    String colonias_IND;
+    String migrantwkrs_IND;
+    String agc_STATUS;
+    String agc_SRC_CD;
+    String counslg_METHOD;
+    String agcid;
+    String email;
+    String fax;
+    String nme;
+    String phone1;
+    String statecd;
+    String weburl;
+    String zipcd;
+    String agc_ADDR_LATITUDE;
+
+
+    public String getServices() {
+        return services;
+    }
+
+    public String getLanguages() {
+        return languages;
+    }
+
+    public String getAdr2() {
+        return adr2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getAdr1() {
+        return adr1;
+    }
+
+    public String getAgc_ADDR_LONGITUDE() {
+        return agc_ADDR_LONGITUDE;
+    }
+
+    public String getParentid() {
+        return parentid;
+    }
+
+    public String getCounty_NME() {
+        return county_NME;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public String getMailingadr1() {
+        return mailingadr1;
+    }
+
+    public String getMailingadr2() {
+        return mailingadr2;
+    }
+
+    public String getMailingcity() {
+        return mailingcity;
+    }
+
+    public String getMailingzipcd() {
+        return mailingzipcd;
+    }
+
+    public String getMailingstatecd() {
+        return mailingstatecd;
+    }
+
+    public String getState_NME() {
+        return state_NME;
+    }
+
+    public String getState_FIPS_CODE() {
+        return state_FIPS_CODE;
+    }
+
+    public String getFaithbased() {
+        return faithbased;
+    }
+
+    public String getColonias_IND() {
+        return colonias_IND;
+    }
+
+    public String getMigrantwkrs_IND() {
+        return migrantwkrs_IND;
+    }
+
+    public String getAgc_STATUS() {
+        return agc_STATUS;
+    }
+
+    public String getAgc_SRC_CD() {
+        return agc_SRC_CD;
+    }
+
+    public String getCounslg_METHOD() {
+        return counslg_METHOD;
+    }
+
+    public String getAgcid() {
+        return agcid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public String getNme() {
+        return nme;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public String getStatecd() {
+        return statecd;
+    }
+
+    public String getWeburl() {
+        return weburl;
+    }
+
+    public String getZipcd() {
+        return zipcd;
+    }
+
+    public String getAgc_ADDR_LATITUDE() {
+        return agc_ADDR_LATITUDE;
+    }
+}
