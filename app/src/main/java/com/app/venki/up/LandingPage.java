7e2e2cef751023acccacece82f39dcb72175db5b
Sam Siu <samsiu@Sams-MacBook-Pro-2.local>
2016-09-29 08:41:36 -0700

    Location: Ask user to turn on GPS

--- app/src/main/java/com/app/venki/up/LandingPage.java
+++ app/src/main/java/com/app/venki/up/LandingPage.java
@@ -3,10 +3,13 @@ package com.app.venki.up;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -33,7 +36,15 @@ import com.app.venki.up.activities.housing.HousingActivity;
 import com.app.venki.up.activities.jobs.JobsActivity;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 public class LandingPage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
@@ -53,6 +64,9 @@ public class LandingPage extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkPermissions();
+        setGoogleApiClient();
+
         setContentView(R.layout.activity_landing_page);
 
         email_address = getIntent().getStringExtra(LoginActivity.EXTRA_MESSAGE);
@@ -72,8 +86,6 @@ public class LandingPage extends AppCompatActivity
         getSupportActionBar().setIcon(R.mipmap.ic_up);
 
 
-        setGoogleApiClient();
-        checkPermissions();
 
         resultReceiver = new AddressResultReceiver(new Handler());
 
@@ -82,6 +94,7 @@ public class LandingPage extends AppCompatActivity
         setGridViewClicker();
 
     }
+
 
     private void setGridViewClicker(){
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -215,6 +228,11 @@ public class LandingPage extends AppCompatActivity
                     .addApi(LocationServices.API)
                     .build();
         }
+
+
+
+
+
     }
 
     private void checkPermissions() {
@@ -238,6 +256,50 @@ public class LandingPage extends AppCompatActivity
     @Override
     protected void onStart() {
         googleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(30 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        //**************************
+        builder.setAlwaysShow(true); //this is the key ingredient
+        //**************************
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                final LocationSettingsStates state = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // All location settings are satisfied. The client can initialize location
+                        // requests here.
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied. But could be fixed by showing the user
+                        // a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(
+                                    LandingPage.this, 1000);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way to fix the
+                        // settings so we won't show the dialog.
+                        break;
+                }
+            }
+        });
+
         super.onStart();
     }
 
